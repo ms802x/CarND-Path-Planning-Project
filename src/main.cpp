a46da66edef62b581f2e2a5fedbbb39e7a48efe4
ms802x <alhejab.ee@gmail.com>
2021-10-22 20:52:52 +0000

    first

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,15 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
 using std::string;
 using std::vector;
+
+int lane = 1;
+double ref_vel = 0;
 
 int main() {
   uWS::Hub h;
@@ -99,6 +103,185 @@ int main() {
            */
 
 
+     // my code block 1
+         
+// first we need to check if we had any previous point, otherwise the car will be stock. 
+           if(previous_path_x.size() > 0) 
+           car_s = end_path_s; 
+           
+          // this step is the prediction, here the data will be provided to the behavior planner 
+            // then the behavior planner based on that will issue its suggestion
+            bool slow_down = false, left_car=false,right_car=false; 
+          //loop through the detected cars
+          for(int i = 0; i < sensor_fusion.size(); i++) {
+            int car_lane;
+            float d = sensor_fusion[i][6]; 
+          
+            //Specifiy the car lane
+            // each lane is 4 m wide so we 
+            if(d > 0 && d < 4) 
+              car_lane = 0;
+            else if(d > 4 && d < 8) 
+              car_lane = 1;
+            else if(d > 8 && d < 12) 
+              car_lane = 2;
+             else 
+              continue ;
+  
+            // calculate speed
+            double vx = sensor_fusion[i][3];
+            double vy = sensor_fusion[i][4];   
+          // the magnitude of speed vector is 
+            double speed =pow(pow(vx,2)+pow(vy,2),0.5);
+            double s_coor =  sensor_fusion[i][5];
+            
+           // S coordinate predicition 
+            s_coor +=((double)speed * 0.02 * previous_path_x.size());
+
+            //set flags to true or false based on the vehicle location .
+               if ( car_lane == lane ) 
+             // Here the car lane = our lane
+             // check if the target car is behind our car
+              slow_down |= s_coor > car_s && s_coor - car_s < 30;
+             else if ( car_lane - lane == -1 ) 
+              // check car on the left lane
+              left_car |= s_coor - 30 < s_coor && car_s + 30 > s_coor;
+            else if ( car_lane - lane == 1 ) 
+              //check car on right lane
+              right_car |= car_s - 30 < s_coor && car_s + 30 > s_coor;
+                   
+           }
+          
+          
+           
+          // Behavioral Planning 
+          double acc = 0.224; 
+          double max_acc = 49.5;
+           // Here only three finite state will be used
+          //Keep Lane, change lane left,right
+          
+          // The behavior planner starts suggestion based on the given predtion
+          if(slow_down) {
+            if(!left_car && lane > 0) 
+              // here we check that our car is not on left lane boundry+no car on the left. 
+              //thus it can go left if car ahead
+              lane--;
+            
+            else if(!right_car && lane < 2) 
+              // here we check that our car is not on right lane boundry+no car on the right. 
+              //thus it can go right if car ahead
+              lane++;
+            else 
+              // just slow down 
+              ref_vel -= acc; 
+            
+              } else {
+            if(lane != 1) 
+              if((lane == 0 && !right_car) || (lane == 2 && !left_car)) 
+                 lane = 1; //back to the center lane
+              
+            
+             if(ref_vel <  max_acc) 
+              ref_vel +=acc;
+            
+          }      
+       
+          
+          // trajectory generatation.
+           vector<double> ptsx,ptsy;
+           double ref_x = car_x, ref_y = car_y, ref_raw = deg2rad(car_yaw),ref_x_1,ref_y_1;
+          
+          // we need to make sure that we have data history (not 1 point only)
+          if(previous_path_x.size()<2){
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
+          }
+          
+          else{
+            
+          // creat a list of xn_-1 and xn
+            ref_x = previous_path_x[previous_path_x.size() - 1];
+            ref_y = previous_path_y[previous_path_x.size() - 1];
+            ref_x_1 =  previous_path_x[previous_path_x.size() - 2];
+            ref_y_1 =  previous_path_y[previous_path_x.size()- 2];
+            ref_raw = atan2(ref_y - ref_y_1, ref_x - ref_x_1);
+
+            ptsx.push_back(ref_x_1);
+            ptsx.push_back(ref_x);
+            ptsy.push_back(ref_y_1);
+            ptsy.push_back(ref_y);
+          }
+                                                            
+          
+                                                         
+vector<double> next_wp0 = getXY(car_s + 30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+vector<double> next_wp1 = getXY(car_s + 60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+vector<double> next_wp2 = getXY(car_s + 90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+                                                            
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
+                   
+          
+         // Shift the points
+          // without this block of code the car starts out of the street 
+   
+          for(int i = 0; i < ptsx.size(); ++i) {
+            double shift_x = ptsx[i] - ref_x;
+            double shift_y = ptsy[i] - ref_y;
+            ptsx[i] = shift_x * cos(0 - ref_raw) - shift_y * sin(0 - ref_raw);
+            ptsy[i] = shift_x * sin(0 - ref_raw) + shift_y * cos(0 - ref_raw);
+          }
+
+           // here we do the line fitting                                  
+          // create a spline
+          tk::spline s;
+          // set (x, y) points to the spline
+          s.set_points(ptsx, ptsy);
+  
+          for(int i = 0; i < previous_path_x.size(); i++) {
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = sqrt(target_x * target_x + target_y * target_y);
+
+          double x_add_on = 0;
+
+          for(int i = 0; i <= 50 -previous_path_x.size(); ++i) {
+            double N = target_dist / (0.02 * ref_vel/2.24);
+            double x_point = x_add_on + target_x / N;
+            double y_point = s(x_point);
+
+            x_add_on = x_point;
+
+            double x_ref = x_point;
+            double y_ref = y_point;
+
+            x_point = x_ref * cos(ref_raw) - y_ref * sin(ref_raw);
+            y_point = x_ref * sin(ref_raw) + y_ref * cos(ref_raw);
+
+            x_point += ref_x;
+            y_point += ref_y;
+
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+          }
+
+          //block 1 end here
+                                                            
+                                                            
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
